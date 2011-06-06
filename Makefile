4001398c247db5dceed078d102f63e88cbf744a9
Jonathan Daugherty <jtd@galois.com>
2011-06-05 19:22:23 -0700

    Build script fixes, misc

--- Makefile
+++ Makefile
@@ -1,12 +1,22 @@
 
-default: app
+CC=g++
 
-app: main.o
-	ld -lsdl main.o -o app
+SDLINC=`sdl-config --cflags`
+SDLLIB=`sdl-config --libs`
+
+SDLFLAGS=$(SDLINC) $(SDLLIB) -framework Cocoa -lSDL_image
+CPPFLAGS=$(SDLINC) -Wall
+
+PROG=app
+OBJECTS=main.o
+EXTRAS=SDLMain.m
+
+$(PROG): $(OBJECTS)
+	$(CC) $(CPPFLAGS) $(SDLFLAGS) $(OBJECTS) $(EXTRAS) -o $(PROG)
 
 %.cpp:
-	g++ -c $@
+	$(CC) $(CPPFLAGS) -c $@
 
 clean:
-	rm *.o
-	rm app
+	rm -f *.o
+	rm -f $(PROG)
--- main.cpp
+++ main.cpp
@@ -1,5 +1,69 @@
 
-int main(void)
+#include <stdlib.h>
+#include "SDL.h"
+#include "SDL_image.h"
+
+void showImage(char *filename, SDL_Surface *screen, int x, int y)
 {
+    SDL_Surface *image;
+    SDL_Rect dest;
+
+    image = IMG_Load(filename);
+    if (image == NULL) {
+        fprintf(stderr, "Couldn't load %s: %s\n", filename, SDL_GetError());
+        return;
+    }
+
+    /* Blit onto the screen surface.
+       The surfaces should not be locked at this point.
+     */
+    dest.x = x;
+    dest.y = y;
+    dest.w = image->w;
+    dest.h = image->h;
+    SDL_BlitSurface(image, NULL, screen, &dest);
+
+    /* Update the changed portion of the screen */
+    SDL_UpdateRects(screen, 1, &dest);
+}
+
+int main(int argc, char **argv)
+{
+    SDL_Surface *screen;
+    SDL_Event event;
+
+    if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    atexit(SDL_Quit);
+
+    screen = SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE);
+    if (screen == NULL) {
+        fprintf(stderr, "Unable to set video mode: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    showImage((char *) "icons.png", screen, 0, 0);
+
+    while (1) {
+        SDL_WaitEvent(&event);
+
+        switch (event.type) {
+        case SDL_KEYDOWN:
+            printf("The %s key was pressed!\n",
+                   SDL_GetKeyName(event.key.keysym.sym));
+            break;
+        case SDL_KEYUP:
+            printf("The %s key was released.\n",
+                   SDL_GetKeyName(event.key.keysym.sym));
+            break;
+        case SDL_QUIT:
+            printf("Some quit stuff happened\n");
+            exit(0);
+        }
+    }
+
     return 0;
 }
