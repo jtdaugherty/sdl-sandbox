c5c051abe54d572ca1262c34afbe14f548808141
Jonathan Daugherty <jtd@galois.com>
2011-06-05 20:45:19 -0700

    More hacking

--- main.cpp
+++ main.cpp
@@ -38,8 +38,6 @@ void ImageGrid::put(SDL_Surface *dest_surf, int dest_x, int dest_y, int cell_x, 
     dest.h = this->cell_height;
 
     SDL_BlitSurface(this->surf, &src, dest_surf, &dest);
-
-    SDL_UpdateRects(dest_surf, 1, &dest);
 }
 
 int main(int argc, char **argv)
@@ -47,7 +45,6 @@ int main(int argc, char **argv)
     SDL_Surface *screen;
     SDL_Surface *image;
     SDL_Event event;
-    SDL_Rect clip;
 
     if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
@@ -56,37 +53,49 @@ int main(int argc, char **argv)
 
     atexit(SDL_Quit);
 
-    screen = SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE);
-    if (screen == NULL) {
+    if (!SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE)) {
         fprintf(stderr, "Unable to set video mode: %s\n", SDL_GetError());
         exit(1);
     }
 
-    clip.x = 34;
-    clip.y = 34;
-    clip.w = 34;
-    clip.h = 34;
-
     image = IMG_Load("graphics/icons.png");
     if (image == NULL) {
         printf("Could not load image: %s\n", SDL_GetError());
-    } else {
-        ImageGrid *g = new ImageGrid(image, 34, 34);
-        g->put(screen, 10, 10, 0, 0);
-        g->put(screen, 50, 50, 0, 1);
+        exit(1);
     }
 
+    ImageGrid *g = new ImageGrid(image, 34, 34);
+    int x, y;
+
+    screen = SDL_GetVideoSurface();
+
     while (1) {
+        SDL_FillRect(screen, NULL, 0x0);
+        g->put(screen, x, y, 0, 0);
+        // We'd call this, but then we'd have to figure out where the
+        // image *was* before it got moved, and where it is now, in
+        // order to ensure that the correct area gets updated.  We
+        // probably don't care, though, since in general we'll just
+        // clear the buffer, draw everything in the right order, and
+        // copy it to the screen without trying to be too clever about
+        // which areas can be skipped safely.
+        //
+        // SDL_UpdateRects(dest_surf, 1, &dest);
+        SDL_Flip(screen);
+
         SDL_WaitEvent(&event);
 
         switch (event.type) {
         case SDL_KEYDOWN:
-            printf("The %s key was pressed!\n",
-                   SDL_GetKeyName(event.key.keysym.sym));
-            break;
-        case SDL_KEYUP:
-            printf("The %s key was released.\n",
-                   SDL_GetKeyName(event.key.keysym.sym));
+            if (event.key.keysym.sym == SDLK_LEFT) {
+                x -= 5;
+            } else if (event.key.keysym.sym == SDLK_RIGHT) {
+                x += 5;
+            } else if (event.key.keysym.sym == SDLK_UP) {
+                y -= 5;
+            } else if (event.key.keysym.sym == SDLK_DOWN) {
+                y += 5;
+            }
             break;
         case SDL_QUIT:
             printf("Some quit stuff happened\n");
