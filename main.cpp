1728a58b095db12ff54bdaf5ee4f05138bf66264
Jonathan Daugherty <jtd@galois.com>
2011-06-05 20:11:58 -0700

    Main: more hacking

--- main.cpp
+++ main.cpp
@@ -1,36 +1,53 @@
 
 #include <stdlib.h>
+#include <stdexcept>
 #include "SDL.h"
 #include "SDL_image.h"
 
-void showImage(char *filename, SDL_Surface *screen, int x, int y)
+class ImageGrid {
+public:
+    ImageGrid(SDL_Surface *surf, int cell_width, int cell_height);
+
+    void put(SDL_Surface *dest, int dest_x, int dest_y, int cell_x, int cell_y);
+
+private:
+    int cell_width, cell_height;
+    SDL_Surface *surf;
+};
+
+ImageGrid::ImageGrid(SDL_Surface *surf, int cell_width, int cell_height)
 {
-    SDL_Surface *image;
+    this->surf = surf;
+    this->cell_width = cell_width;
+    this->cell_height = cell_height;
+}
+
+void ImageGrid::put(SDL_Surface *dest_surf, int dest_x, int dest_y, int cell_x, int cell_y)
+{
     SDL_Rect dest;
+    SDL_Rect src;
 
-    image = IMG_Load(filename);
-    if (image == NULL) {
-        fprintf(stderr, "Couldn't load %s: %s\n", filename, SDL_GetError());
-        return;
-    }
+    src.x = cell_x * this->cell_width;
+    src.y = cell_y * this->cell_height;
+    src.w = this->cell_width;
+    src.h = this->cell_height;
 
-    /* Blit onto the screen surface.
-       The surfaces should not be locked at this point.
-     */
-    dest.x = x;
-    dest.y = y;
-    dest.w = image->w;
-    dest.h = image->h;
-    SDL_BlitSurface(image, NULL, screen, &dest);
+    dest.x = dest_x;
+    dest.y = dest_y;
+    dest.w = this->cell_width;
+    dest.h = this->cell_height;
 
-    /* Update the changed portion of the screen */
-    SDL_UpdateRects(screen, 1, &dest);
+    SDL_BlitSurface(this->surf, &src, dest_surf, &dest);
+
+    SDL_UpdateRects(dest_surf, 1, &dest);
 }
 
 int main(int argc, char **argv)
 {
     SDL_Surface *screen;
+    SDL_Surface *image;
     SDL_Event event;
+    SDL_Rect clip;
 
     if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
@@ -45,7 +62,19 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    showImage((char *) "graphics/icons.png", screen, 0, 0);
+    clip.x = 34;
+    clip.y = 34;
+    clip.w = 34;
+    clip.h = 34;
+
+    image = IMG_Load("graphics/icons.png");
+    if (image == NULL) {
+        printf("Could not load image: %s\n", SDL_GetError());
+    } else {
+        ImageGrid *g = new ImageGrid(image, 34, 34);
+        g->put(screen, 10, 10, 0, 0);
+        g->put(screen, 50, 50, 0, 1);
+    }
 
     while (1) {
         SDL_WaitEvent(&event);
